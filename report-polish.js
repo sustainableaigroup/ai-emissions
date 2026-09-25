a32effb6c5c32c2f874e029a3be97d08c2ff21d7
Boris Gamazaychikov <136664511+bgamazay@users.noreply.github.com>
2026-09-25 10:51:09 +0200

    adding new report contents

--- report-polish.js
+++ report-polish.js
@@ -4,7 +4,7 @@
 (() => {
   const links = {
     dashboardUrl: "#",
-    githubUrl: "https://github.com/sustainableaigroup/ai-emissions/issues",
+    githubUrl: "https://github.com/sustainableaigroup/CLEER-Tech-Report/issues",
     contactUrl: "https://sustainableaigroup.com/#contact"
   };
 
